8e79df05b1206a2ab014824831a46b23d22ee253
highway92 <highway92@kakao.com>
2024-10-17 16:36:12 +0900

    player object create

---
+++ src/main/java/com/prime/player/Player.java
@@ -0,0 +1,41 @@
+package com.prime.player;
+
+import com.prime.cardDeck.Card;
+import com.prime.cardDeck.CardValue;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class Player {
+    private final String name;
+    private final List<Card> hand = new ArrayList<>();
+    private boolean done = false;
+
+    public Player(String name, Card firstCard, Card secondCard) {
+        this.name = name;
+        hand.add(firstCard);
+        hand.add(secondCard);
+    }
+
+    public void hit(Card card) {
+        hand.add(card);
+    }
+
+    public void stay() {
+        this.done = true;
+    }
+
+    public boolean isBusted() {
+        Integer handSum = this.hand.stream()
+                .map(Card::getCardValue)
+                .map(CardValue::getValue)
+                .reduce(0,Integer::sum);
+        return handSum > 21;
+    }
+
+    public List<Card> getHand() {
+        return List.copyOf(hand);
+    }
+}
